41099ff3e03ad5509ee15afb083910dfdeb77827
MrFizban <fizban94@gmail.com>
2021-08-16 16:41:33 +0200

    init

---
+++ Extension.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.Dto;
+using Catalog.Entities;
+
+namespace Catalog
+{
+    public static class Extension
+    {
+        public static ItemDto AsDto(this Item item)
+        {
+            return new ItemDto
+            {
+                id = item.id,
+                name = item.name,
+                price = item.price,
+                CreateData = item.CreateData
+            };
+        }
+    }
+}
